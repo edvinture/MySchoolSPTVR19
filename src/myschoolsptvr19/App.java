48ceb44c8dc960fb8ed01e4cab8d57117e0e66cc
Edvin Kaapa <edvin.kaapa@ivkhk.ee>
2020-10-21 10:26:20 +0300

    zagotovka dlua MySchool

---
+++ src/myschoolsptvr19/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myschoolsptvr19;
+
+/**
+ *
+ * @author user
+ */
+class App {
+
+    public void run() {
+        System.out.println("---Моя школа---");
+       
+    }
+    
+}
